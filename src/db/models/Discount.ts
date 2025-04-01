62363d5e67c76783eb041c98457b05f400d3ee9c
skaredko <shader228@gmail.com>
2025-04-01 13:08:59 +0200

    fix models discount and payment

--- src/db/models/Discount.ts
+++ src/db/models/Discount.ts
@@ -1,110 +1,42 @@
 import {
-	Association,
 	CreationOptional,
 	DataTypes,
 	InferCreationAttributes,
 	InferAttributes,
 	Model,
-	NonAttribute,
 	Sequelize,
-	Transaction,
-	HasManyAddAssociationMixin,
-	HasManyAddAssociationsMixin,
-	HasManyCountAssociationsMixin,
-	HasManyCreateAssociationMixin,
-	HasManyGetAssociationsMixin,
-	HasManyHasAssociationMixin,
-	HasManyHasAssociationsMixin,
-	HasManyRemoveAssociationMixin,
-	HasManyRemoveAssociationsMixin,
-	HasManySetAssociationsMixin,
-	HasOneCreateAssociationMixin,
-	HasOneGetAssociationMixin,
-	HasOneSetAssociationMixin
-} from 'sequelize';
-import logger from '../../logger';
-import db from '../db';
-import { DiscountType, discountTypes, PriceableType } from '../types';
-import { BadRequestError } from '../../errors';
-import { Order, OrderInterface } from './Order';
-import { Product } from './Product';
-import { Price, PriceInterface } from './Price';
-import { fetchSingleData, fetchMultiData } from '../helper';
+} from 'sequelize'
+import { DiscountType, discountTypes } from '../types'
+import { BaseModelInterface } from './models'
 
-interface DiscountBaseInterface {
-	id: string;
-
-	type: DiscountType;
-	value?: number;
-
-	createdAt: Date;
-	updatedAt: Date;
-	deletedAt?: Date;
+interface DiscountBaseInterface extends BaseModelInterface {
+	type: DiscountType
+	value?: number
 }
 
-interface DiscountAssociationsInterface {
-	price: PriceInterface | string;
-	orders: OrderInterface[] | string[];
-	products: any[] | string[];
-}
+interface DiscountAssociationsInterface {}
 
 export interface DiscountInterface
 	extends DiscountBaseInterface,
-	DiscountAssociationsInterface { }
+		DiscountAssociationsInterface {}
 
-type DiscountAssociations = 'orders' | 'products' | 'price';
+type DiscountAssociations = ''
 
 export class Discount extends Model<
 	InferAttributes<Discount, { omit: DiscountAssociations }>,
 	InferCreationAttributes<Discount, { omit: DiscountAssociations }>
 > {
-	declare id: CreationOptional<string>;
+	declare id: CreationOptional<string>
 
-	declare type: CreationOptional<DiscountType>;
+	declare type: CreationOptional<DiscountType>
 
-	declare value: CreationOptional<number>;
+	declare value: CreationOptional<number>
 
-	declare createdAt: CreationOptional<Date>;
-	declare updatedAt: CreationOptional<Date>;
-	declare deletedAt: CreationOptional<Date>;
+	declare createdAt: CreationOptional<Date>
+	declare updatedAt: CreationOptional<Date>
+	declare deletedAt: CreationOptional<Date>
 
-	// Discount belongsTo Price
-	declare price?: NonAttribute<Price>;
-	declare getPrice: HasOneGetAssociationMixin<Price>;
-	declare setPrice: HasOneSetAssociationMixin<Price, string>;
-	declare createPrice: HasOneCreateAssociationMixin<Price>;
-
-	// Discount hasMany Orders
-	declare orders?: NonAttribute<Order[]>;
-	declare getOrders: HasManyGetAssociationsMixin<Order>;
-	declare setOrders: HasManySetAssociationsMixin<Order, string>;
-	declare addOrder: HasManyAddAssociationMixin<Order, string>;
-	declare addOrders: HasManyAddAssociationsMixin<Order, string>;
-	declare createOrder: HasManyCreateAssociationMixin<Order>;
-	declare removeOrder: HasManyRemoveAssociationMixin<Order, string>;
-	declare removeOrders: HasManyRemoveAssociationsMixin<Order, string>;
-	declare hasOrder: HasManyHasAssociationMixin<Order, string>;
-	declare hasOrders: HasManyHasAssociationsMixin<Order, string>;
-	declare countOrders: HasManyCountAssociationsMixin;
-
-	// Discount hasMany Products
-	declare products?: NonAttribute<Product[]>;
-	declare getProducts: HasManyGetAssociationsMixin<Product>;
-	declare setProducts: HasManySetAssociationsMixin<Product, string>;
-	declare addProduct: HasManyAddAssociationMixin<Product, string>;
-	declare addProducts: HasManyAddAssociationsMixin<Product, string>;
-	declare createProduct: HasManyCreateAssociationMixin<Product>;
-	declare removeProduct: HasManyRemoveAssociationMixin<Product, string>;
-	declare removeProducts: HasManyRemoveAssociationsMixin<Product, string>;
-	declare hasProduct: HasManyHasAssociationMixin<Product, string>;
-	declare hasProducts: HasManyHasAssociationsMixin<Product, string>;
-	declare countProducts: HasManyCountAssociationsMixin;
-
-	declare static associations: {
-		price: Association<Discount, Price>;
-		orders: Association<Discount, Order>;
-		products: Association<Discount, Product>;
-	};
+	declare static associations: {}
 
 	static initModel(sequelize: Sequelize): typeof Discount {
 		Discount.init(
@@ -114,164 +46,78 @@ export class Discount extends Model<
 					primaryKey: true,
 					allowNull: false,
 					unique: true,
-					defaultValue: DataTypes.UUIDV4
+					defaultValue: DataTypes.UUIDV4,
 				},
 				type: {
 					type: DataTypes.STRING,
 					allowNull: false,
 					validate: {
-						isIn: [discountTypes]
+						isIn: [discountTypes],
 					},
-					defaultValue: DiscountType.fixed
+					defaultValue: DiscountType.fixed,
 				},
 				value: {
 					type: DataTypes.INTEGER,
 					allowNull: true,
 					validate: {
 						isDecimal: {
-							msg: 'Value must be a number'
+							msg: 'Value must be a number',
 						},
 						min: {
 							args: [0],
-							msg: 'Value must be greater than or equal to 0'
+							msg: 'Value must be greater than or equal to 0',
 						},
 						max: {
 							args: [100],
-							msg: 'Value must be less than or equal to 100'
-						}
-					}
+							msg: 'Value must be less than or equal to 100',
+						},
+					},
 				},
 				createdAt: {
-					type: DataTypes.DATE
+					type: DataTypes.DATE,
 				},
 				updatedAt: {
-					type: DataTypes.DATE
+					type: DataTypes.DATE,
 				},
 				deletedAt: {
 					type: DataTypes.DATE,
-					allowNull: true
-				}
+					allowNull: true,
+				},
 			},
 			{
 				sequelize,
 				paranoid: true,
-				hooks: {
-					beforeValidate(discount) {
-						if (discount.type === DiscountType.fixed) {
-							if (!discount.price) {
-								throw new BadRequestError(
-									'Price is required for discount that is fixed price'
-								);
-							}
-						} else if (discount.type === DiscountType.percentage) {
-							if (!discount.value) {
-								throw new BadRequestError(
-									'Value is required for discount that is percentage'
-								);
-							}
-						}
-					}
-				}
 			}
-		);
+		)
 
-		return Discount;
+		return Discount
 	}
 
-	static associate() {
-		// Discount has no deps
+	static associate() {}
 
-		Discount.hasMany(Order, {
-			foreignKey: 'discountId'
-		});
-
-		Discount.hasMany(Product, {
-			foreignKey: 'discountId'
-		});
-
-		Discount.hasOne(Price, {
-			foreignKey: 'discountId',
-			scope: {
-				priceableType: PriceableType.discount
-			}
-		});
-	}
-
-	public async data(dto: boolean = true): Promise<DiscountInterface> {
+	public async data(): Promise<DiscountInterface> {
 		const fields = [
 			'id',
 			'type',
 			'value',
 			'createdAt',
 			'updatedAt',
-			...(this.deletedAt ? ['deletedAt'] : [])
-		];
+			...(this.deletedAt ? ['deletedAt'] : []),
+		]
 
 		const base_data = fields.reduce((acc, field) => {
 			return {
 				...acc,
-				[field]: this[field as keyof Discount]
-			};
-		}, {}) as DiscountInterface;
+				[field]: this[field as keyof Discount],
+			}
+		}, {}) as DiscountInterface
 
-		const [price, orders, products] = await Promise.all([
-			fetchSingleData<PriceInterface, Price>(() => this.getPrice(), dto),
-			fetchMultiData<OrderInterface, Order>(() => this.getOrders(), dto),
-			fetchMultiData<any, Product>(() => this.getProducts(), dto)
-		]);
-
-		if (price === undefined) {
-			throw new Error('Price not found');
-		}
-
-		const associated_data: DiscountAssociationsInterface = {
-			price,
-			orders: orders as OrderInterface[] | string[],
-			products: products as any[] | string[]
-		};
+		const associated_data: DiscountAssociationsInterface = {}
 
 		return {
 			...base_data,
 
-			...associated_data
-		};
-	}
-
-	public async delete(options?: {
-		force?: boolean;
-		transaction?: Transaction;
-	}): Promise<void> {
-		try {
-			const force = options?.force ?? false;
-			const transaction = options?.transaction;
-
-			if (force) {
-				if (transaction) {
-					await this.cleanUp({ force, transaction });
-				} else {
-					await db.transaction(async (transaction: Transaction) => {
-						await this.cleanUp({ force, transaction });
-					});
-				}
-			} else {
-				if (transaction) {
-					await this.destroy({ transaction });
-				} else {
-					await db.transaction(async (transaction: Transaction) => {
-						await this.destroy({ transaction });
-					});
-				}
-			}
-		} catch (err: unknown) {
-			logger.error('Delete Address error, ', err);
-			throw err;
+			...associated_data,
 		}
-	}
-
-	public async cleanUp(options: {
-		force: boolean;
-		transaction: Transaction;
-	}): Promise<void> {
-		await this.destroy(options);
 	}
 }
--- src/db/models/Payment.ts
+++ src/db/models/Payment.ts
@@ -7,70 +7,49 @@ import {
 	Model,
 	NonAttribute,
 	Sequelize,
-	Transaction,
 	BelongsToCreateAssociationMixin,
 	BelongsToGetAssociationMixin,
 	BelongsToSetAssociationMixin,
-	HasOneCreateAssociationMixin,
-	HasOneGetAssociationMixin,
-	HasOneSetAssociationMixin
-} from 'sequelize';
-import { fetchSingleData, validateStringField } from '../helper';
-import logger from '../../logger';
-import db from '../db';
-import { Order, OrderInterface } from './Order';
-import { Price, PriceInterface } from './Price';
-import { PriceableType } from '../types';
+} from 'sequelize'
+import { fetchSingleData, validateStringField } from '../helper'
+import { Order, OrderInterface } from './Order'
+import { BaseModelInterface } from './models'
 
-interface PaymentBaseInterface {
-	id: string;
-
-	stripeId: string;
-
-	createdAt: Date;
-	updatedAt: Date;
-	deletedAt?: Date;
+interface PaymentBaseInterface extends BaseModelInterface {
+	stripeId: string
 }
 
 interface PaymentAssociationsInterface {
-	price: PriceInterface | string;
-	order: OrderInterface | string;
+	order: OrderInterface | string
 }
 
 export interface PaymentInterface
 	extends PaymentBaseInterface,
 		PaymentAssociationsInterface {}
 
-type PaymentAssociations = 'price' | 'order';
+type PaymentAssociations = 'order'
 
 export class Payment extends Model<
 	InferAttributes<Payment, { omit: PaymentAssociations }>,
 	InferCreationAttributes<Payment, { omit: PaymentAssociations }>
 > {
-	declare id: CreationOptional<string>;
+	declare id: CreationOptional<string>
 
-	declare stripeId: CreationOptional<string>;
+	declare stripeId: CreationOptional<string>
 
-	declare createdAt: CreationOptional<Date>;
-	declare updatedAt: CreationOptional<Date>;
-	declare deletedAt: CreationOptional<Date>;
-
-	// Payment hasOne Price
-	declare price?: NonAttribute<Price>;
-	declare getPrice: HasOneGetAssociationMixin<Price>;
-	declare setPrice: HasOneSetAssociationMixin<Price, string>;
-	declare createPrice: HasOneCreateAssociationMixin<Price>;
+	declare createdAt: CreationOptional<Date>
+	declare updatedAt: CreationOptional<Date>
+	declare deletedAt: CreationOptional<Date>
 
 	// Payment belongsTo Order
-	declare order?: NonAttribute<Order>;
-	declare getOrder: BelongsToGetAssociationMixin<Order>;
-	declare setOrder: BelongsToSetAssociationMixin<Order, string>;
-	declare createOrder: BelongsToCreateAssociationMixin<Order>;
+	declare order?: NonAttribute<Order>
+	declare getOrder: BelongsToGetAssociationMixin<Order>
+	declare setOrder: BelongsToSetAssociationMixin<Order, string>
+	declare createOrder: BelongsToCreateAssociationMixin<Order>
 
 	declare static associations: {
-		price: Association<Payment, Price>;
-		order: Association<Payment, Order>;
-	};
+		order: Association<Payment, Order>
+	}
 
 	static initModel(sequelize: Sequelize): typeof Payment {
 		Payment.init(
@@ -80,7 +59,7 @@ export class Payment extends Model<
 					primaryKey: true,
 					allowNull: false,
 					unique: true,
-					defaultValue: DataTypes.UUIDV4
+					defaultValue: DataTypes.UUIDV4,
 				},
 				stripeId: {
 					type: DataTypes.STRING,
@@ -89,42 +68,35 @@ export class Payment extends Model<
 						isString: validateStringField('stripeId'),
 						len: {
 							args: [0, 1024],
-							msg: 'Name max length is 1024 characters'
-						}
-					}
+							msg: 'Name max length is 1024 characters',
+						},
+					},
 				},
 				createdAt: {
-					type: DataTypes.DATE
+					type: DataTypes.DATE,
 				},
 				updatedAt: {
-					type: DataTypes.DATE
+					type: DataTypes.DATE,
 				},
 				deletedAt: {
 					type: DataTypes.DATE,
-					allowNull: true
-				}
+					allowNull: true,
+				},
 			},
 			{
 				sequelize,
-				paranoid: true
+				paranoid: true,
 			}
-		);
+		)
 
-		return Payment;
+		return Payment
 	}
 
 	static associate() {
-		Payment.hasOne(Price, {
-			foreignKey: 'paymentId',
-			scope: {
-				priceableType: PriceableType.payment
-			}
-		});
-
 		Payment.belongsTo(Order, {
 			foreignKey: 'orderId',
-			onDelete: 'CASCADE'
-		});
+			onDelete: 'CASCADE',
+		})
 	}
 
 	public async data(dto: boolean = true): Promise<PaymentInterface> {
@@ -133,76 +105,32 @@ export class Payment extends Model<
 			'stripeId',
 			'createdAt',
 			'updatedAt',
-			...(this.deletedAt ? ['deletedAt'] : [])
-		];
+			...(this.deletedAt ? ['deletedAt'] : []),
+		]
 
 		const base_data = fields.reduce((acc, field) => {
 			return {
 				...acc,
-				[field]: this[field as keyof Payment]
-			};
-		}, {}) as PaymentBaseInterface;
+				[field]: this[field as keyof Payment],
+			}
+		}, {}) as PaymentBaseInterface
 
-		const [price, order] = await Promise.all([
-			fetchSingleData<PriceInterface, Price>(() => this.getPrice(), dto),
-			fetchSingleData<OrderInterface, Order>(() => this.getOrder(), dto)
-		]);
-
-		if (price === undefined) {
-			throw new Error('Price not found');
-		}
+		const [order] = await Promise.all([
+			fetchSingleData<OrderInterface, Order>(() => this.getOrder(), dto),
+		])
 
 		if (order === undefined) {
-			throw new Error('Order not found');
+			throw new Error('Order not found')
 		}
 
 		const associated_data: PaymentAssociationsInterface = {
-			price,
-			order
-		};
+			order,
+		}
 
 		return {
 			...base_data,
 
-			...associated_data
-		};
-	}
-
-	public async delete(options?: {
-		force?: boolean;
-		transaction?: Transaction;
-	}): Promise<void> {
-		try {
-			const force = options?.force ?? false;
-			const transaction = options?.transaction;
-
-			if (force) {
-				if (transaction) {
-					await this.cleanUp({ force, transaction });
-				} else {
-					await db.transaction(async (transaction: Transaction) => {
-						await this.cleanUp({ force, transaction });
-					});
-				}
-			} else {
-				if (transaction) {
-					await this.destroy({ transaction });
-				} else {
-					await db.transaction(async (transaction: Transaction) => {
-						await this.destroy({ transaction });
-					});
-				}
-			}
-		} catch (err: unknown) {
-			logger.error('Delete Address error, ', err);
-			throw err;
+			...associated_data,
 		}
-	}
-
-	public async cleanUp(options: {
-		force: boolean;
-		transaction: Transaction;
-	}): Promise<void> {
-		await this.destroy(options);
 	}
 }
